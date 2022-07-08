d620a42c809f8ccc1490c048dcaa561133f5ea63
Erika Hamilton <hamiltone@g.cofc.edu>
2022-07-08 13:57:27 -0400

    fix

--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
@@ -1,5 +1,9 @@
 package guru.springframework.spring5jokesappv2.services;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
 public interface JokeService {
 
     String getJoke();
--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -1,15 +1,23 @@
 package guru.springframework.spring5jokesappv2.services;
-import guru.springframework.norris.chuck.*;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    private final ChuckNorrisQuotes chuckNorrisQuotes;
-    
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+  
+
+
+   
+
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
 
